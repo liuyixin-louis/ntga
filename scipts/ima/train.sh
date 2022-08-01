be01ae144d737a82f9d62d5b2a76565738995042
Yixin Liu <53036760+liuyixin-louis@users.noreply.github.com>
2022-08-01 17:53:04 +0800

    upload

--- scipts/ima/train.sh
+++ scipts/ima/train.sh
@@ -2,4 +2,4 @@ cd $( dirname $(dirname  $(dirname $0)))
 dataset="imagenet"
 exp_name="NTGA-$dataset"
 mkdir -p ./exp_data/exp_name
-python generate_attack.py --model_type fnn --dataset $dataset --save_path ./exp_data/exp_name --eps 8/255
+python generate_attack.py --model_type fnn --dataset $dataset --save_path ./exp_data/exp_name --eps 0.03137254901960784
