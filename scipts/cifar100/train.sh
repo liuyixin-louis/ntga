4e77def8491b33440cdc50692c52b811e95a67e7
Yixin Liu <53036760+liuyixin-louis@users.noreply.github.com>
2022-08-01 17:49:27 +0800

    upload

--- scipts/cifar100/train.sh
+++ scipts/cifar100/train.sh
@@ -1,5 +1,5 @@
 cd $( dirname $(dirname  $(dirname $0)))
-dataset="cifar10"
+dataset="cifar100"
 exp_name="NTGA-$dataset"
 mkdir -p ./exp_data/exp_name
 python generate_attack.py --model_type fnn --dataset $dataset --save_path ./exp_data/exp_name --eps 8/255
